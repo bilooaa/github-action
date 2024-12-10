ab250443a6ede34bfb23669aef6785b0539747ca
bilooaa <ashish_bhargava85@yahoo.com>
2024-12-10 12:09:28 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 def print_colors():
-  colors_list - ["red", "blue", "green"]
-  for i in colors_list:
-    print(i)
+    colors_list = ["red", "blue", "green"]  # Corrected the assignment
+    for color in colors_list:
+        print(color)
+
 print_colors()
-  
